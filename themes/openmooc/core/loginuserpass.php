b07a597ba7273e4aad45f13483c97b472b450c07
root <root@idp.openmooc.org>
2012-08-13 12:17:13 +0200

    Adding basic alert message style

--- themes/openmooc/core/loginuserpass.php
+++ themes/openmooc/core/loginuserpass.php
@@ -23,10 +23,8 @@ $this->langtext = array_merge($this->langtext, $lang);
 <?php
 if ($this->data['errorcode'] !== NULL) {
 ?>
-	<div style="border-left: 1px solid #e8e8e8; border-bottom: 1px solid #e8e8e8; background: #f5f5f5">
-		<img src="/<?php echo $this->data['baseurlpath']; ?>resources/icons/experience/gtk-dialog-error.48x48.png" class="float-l" style="margin: 15px " />
-		<h2><?php echo $this->t('{login:error_header}'); ?></h2>
-		<p><b><?php echo $this->t('{errors:title_' . $this->data['errorcode'] . '}'); ?></b></p>
+	<div class="alert alert-error">		
+		<h4 class="alert-heading"><?php echo $this->t('{errors:title_' . $this->data['errorcode'] . '}'); ?></h4>
 		<p><?php echo $this->t('{errors:descr_' . $this->data['errorcode'] . '}'); ?></p>
 	</div>
 <?php
